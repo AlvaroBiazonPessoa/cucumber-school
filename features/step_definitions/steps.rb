02f687a482922a3a1a2b733a6f5b8cef1dd74ea0
Alvaro Biazon Pessoa <alvarobiazon@hotmail.com>
2022-08-17 18:08:03 -0300

    Foi add steps.rb para defini de steps

---
+++ features/step_definitions/steps.rb
@@ -0,0 +1,11 @@
+Given('Lucy is located {int} metres from Sean') do |distance|
+    puts distance
+end
+    
+When('Sean shouts {string}') do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+end
+    
+Then('Lucy hear Seans message') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
